eaa17c1004028d023f913a58edbddb37e63fd0b2
Shashank Mishra <mishrashashank2106@gmail.com>
2025-08-10 23:59:06 +0530

    common package and zod schema added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,27 +2,47 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
-
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
 const app = express();
 
 app.post("/signup", (req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.json({
+            message: "Incorrect Inputs"
+        })
+    }
     res.json({
-        userId: "123" 
+        userId: "123"
     })
 })
 
 app.post("/signin", (req, res) => {
-    
+
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.json({
+            message: "Incorrect Inputs"
+        })
+    }
     const userId = 1;
     const token = jwt.sign({
         userId
-    }, JWT_SECRET ); 
+    }, JWT_SECRET);
 
     res.json({
         token
     })
 })
-app.post("/create-room", middleware , (req, res) => {
+app.post("/create-room", middleware, (req, res) => {
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.json({
+            message: "Incorrect Inputs"
+        })
+    }
     res.json({
         roomId: 123
     })
