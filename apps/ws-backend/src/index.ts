7bb1642dfc0e9f74b1532e936b45fc7d1b126bfc
Shashank Mishra <mishrashashank2106@gmail.com>
2025-08-10 23:20:48 +0530

    addwd backend common in turborepo

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from 'ws';
 import jwt, { JwtPayload }  from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -10,12 +10,17 @@ wss.on('connection', function connection(ws, request) {
     if(!url){
         return;
     }
-    
+
     const queryParams = new URLSearchParams(url.split("?")[1])
     const token = queryParams.get('token') || "";
     const decoded  = jwt.verify(token, JWT_SECRET) ;
 
-        if(!decoded || !(decoded as JwtPayload).userId){
+        if (typeof decoded == "string"){
+            ws.close();
+            return;
+        }
+
+        if(!decoded || ! decoded.userId){
             ws.close();
             return;     
         }
