a5a22247a4aa29c78895e71fcb9e907e692e8e29
Shashank Mishra <mishrashashank2106@gmail.com>
2025-08-10 21:52:02 +0530

    done bussiness logic of signin signup , done middlewware and ws entry

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,34 @@
-import express  from "express";
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
-app.listen(3000);
+app.post("/signup", (req, res) => {
+    res.json({
+        userId: "123" 
+    })
+})
+
+app.post("/signin", (req, res) => {
+
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET ); 
+
+    res.json({
+        token
+    })
+})
+app.post("/create-room", middleware , (req, res) => {
+    res.json({
+        roomId: 123
+    })
+})
+
+
+app.listen(3001);
 
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,26 @@
+import {NextFunction, Request , Response} from "express";
+import { JWT_SECRET } from "./config";
+import  jwt  from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  userId?: string;
+}
+
+interface MyJwtPayload extends jwt.JwtPayload {
+  userId: string;
+}
+
+
+export function middleware(req : AuthRequest, res : Response, next : NextFunction){
+        const token = req.headers["authorization"] ?? "";
+        const decoded = jwt.verify(token , JWT_SECRET) as MyJwtPayload;
+
+        if(decoded){
+            req.userId = decoded.userId;
+        }else{
+            res.status(403).json({
+                message: "Unathorized"
+            })
+        }
+
+    }
